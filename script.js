8fa0c8ccd4020b2f10ec85b225683436ac671f14
Durga-velu <durgadevi134babu@gmail.com>
2025-03-29 00:22:15 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
 //your JS code here. If required.
+
+function removeBtn(){
+	
+	let selectedTag=document.getElementById("colorSelect")
+
+
+	selectedTag.remove(selectedTag.selectedIndex)
+}
+	
+})
+
